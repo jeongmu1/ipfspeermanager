1761fbbadd6b007463986b75dfd89557b8184e56
Jeongmu <Kang815595@gmail.com>
2024-04-18 10:50:33 +0900

    chore: 프로젝트 생성

---
+++ src/main/java/com/dsu/ipfspeermanager/IpfspeermanagerApplication.java
@@ -0,0 +1,13 @@
+package com.dsu.ipfspeermanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpfspeermanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpfspeermanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dsu/ipfspeermanager/IpfspeermanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsu.ipfspeermanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpfspeermanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
